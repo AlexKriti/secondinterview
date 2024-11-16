16edbe22b83a61f64f565e556bb93c12a5431da5
AlexKriti <103134968+AlexKriti@users.noreply.github.com>
2024-11-16 18:12:46 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@ let emailInput = document.querySelector('.email-inp input')
 let phoneInput = document.querySelector('.phone-inp input')
 let textInpArr = document.querySelectorAll('form input.text-input');
 let formError = document.querySelector('main .registration-form .fill-error');
-
-
+let cookieBlock = document.querySelector('.cookie-wrap');
+let cookieBut = document.querySelector('.cookie button');
 
 
 localStorage.cookieAgree = 'no';
@@ -17,48 +17,28 @@ localStorage.cookieAgree = 'no';
 //     document.querySelector('body').removeChild(document.querySelector('.cookie-wrap'))
 // }
 
-// cookieBut.addEventListener('click', (e) => {
-//     console.log(localStorage.cookieAgree);
-//     if(localStorage.cookieAgree == 'no'){
-//         localStorage.cookieAgree = 'yes' ;
-//         cookieBlock.style.display = ' none';
-//     }
-//     console.log(localStorage.cookieAgree);
-// })
+cookieBut.addEventListener('click', (e) => {
+    localStorage.cookieAgree = 'yes' ;
+    cookieBlock.style.display = ' none';
+})
 
-document.addEventListener("DOMContentLoaded", function() {
-    let cookieBlock = document.querySelector('.cookie-wrap');
-    let cookieBut = document.querySelector('.cookie button');
 
-    // Проверяем, было ли принято решение о скрытии баннера
-    if (localStorage.getItem("cookiesAccepted") === "true") {
-        cookieBlock.style.display = "none"; // Скрываем баннер
+
+
+
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let name = nameInput.value;
+    let email = emailInput.value;
+    let phone = phoneInput.value;
+    if(!name || !email || !phone){
+        formError.style.display = 'block';
+        console.log('empty error')
+        return;
     }
 
-    // Обработчик события для кнопки
-    cookieBut.addEventListener("click", function() {
-        localStorage.setItem("cookiesAccepted", "true"); // Сохраняем решение
-        cookieBlock.style.display = "none"; // Скрываем баннер
-    });
-});
-
-
-
-
-
-// myForm.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     let name = nameInput.value;
-//     let email = emailInput.value;
-//     let phone = phoneInput.value;
-//     if(!name || !email || !phone){
-//         formError.style.display = 'block';
-//         console.log('empty error')
-//         return;
-//     }
-
-//     window.location.href = 'success.html'
-// })
+    window.location.href = 'success.html'
+})
 
 
 
